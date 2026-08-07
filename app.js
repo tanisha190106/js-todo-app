117b4ffb2dc4089b3d3c6d04637cd7642393a50b
Tanisha Agarwal <tanisha.agarwal0109@gmail.com>
2026-08-08 01:17:07 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+let todo = [];
+let req = prompt("Please enter your request:");
+while(true){
+  if(req == "quit"){
+    console.log("quitting app");
+    break;
+  }
+  if(req == "list"){
+    console.log("-------------------");
+    for(let i = 0; i < todo.length; i++){
+      console.log(i, todo[i]);
+    }
+    console.log("-------------------");
+  }else if(req == "add"){
+    let task = prompt("Please enter the task you want to add:");
+    todo.push(task);
+    console.log(`${task} added.`);
+  }else if(req == "delete"){
+    let idx = prompt("Please enter the task index:");
+    if(idx >= 0 && idx < todo.length){
+      todo.splice(idx,1);
+    }else{
+      console.log("Invalid Index");
+    }
+  }else{
+    console.log("Wrong Request");
+  }
+  req = prompt("Please enter your request:");
+}
